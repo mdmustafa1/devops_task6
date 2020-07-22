03c5d26e9d36e8814e1d2859f72ced25087dc1cb
Mustafa <54718866+mdmustafa1@users.noreply.github.com>
2020-07-22 19:18:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <body bgcolor='blue'>
 <?php
-echo "<center><h1>Hi! It is your default php home page</h1></center>";
+echo "<center><h1>It is your default php home page</h1></center>";
  ?>
  </body>
