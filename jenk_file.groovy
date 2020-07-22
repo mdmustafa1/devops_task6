4b49eb55fa2e9e0ab9f4f78c5796875d853db33b
Mustafa <54718866+mdmustafa1@users.noreply.github.com>
2020-07-22 16:51:15 +0530

    cjf8

--- jenk_file.groovy
+++ jenk_file.groovy
@@ -1,6 +1,6 @@
 //starting the code for job1 building the source code as container and pushing to docker hub
 
-job('job1_pull_build'){
+job('job1_pullbuild'){
      scm{
              github('https://github.com/mdmustafa1/devops_task6.git')      
          } 
@@ -17,12 +17,39 @@ job('job1_pull_build'){
 //code for job2 deployment on kubernetes using various resourcses
 job('job2_deploy'){
      triggers{
-            upstream('job1_pull_build','SUCCESS')
+            upstream('job1_pullbuild','SUCCESS')
          }
      steps{
  shell('cd /var/lib/jenkins/workspace/job2_seed/; if (ls | grep .html); then; if(sudo kubectl get deploy | grep htmldeploy); then; echo "html env already running"; kubectl rollout restart deploy/htmldeploy; kubectl rollout status deploy/htmldeploy; else; sudo kubectl apply -k /task3/html; fi; fi; if (ls | grep .php); then; if(sudo kubectl get deploy | grep phpdeploy); then; echo "php env already running"; kubectl rollout restart deploy/phpdeploy; kubectl rollout status deploy/phpdeploy; else; sudo kubectl apply -k /task3/php; fi; fi')
         }}
 
+
+
+
+ // job3 for monitoring the job and sending the email to developer      
+       
+       job('job3_test_monitor'){
+     triggers{
+           upstream('job2_deploy','SUCCESS')
+         }
+    steps{
+            shell('status=$(curl -o /dev/null -s -w "%{http_code}" 192.168.99.100:81); if [[ $status = 200 ]]; then; exit 0; else; exit 1; fi; status=$(curl -o /dev/null -s -w "%{http_code}" 192.168.99.100:82); if [[ $status = 200 ]]; then; exit 0; else; exit 1; fi')
+       }
+     publishers {
+        extendedEmail {
+            recipientList('mdmustafahusain4u@gmail.com')
+    
+            triggers {
+     
+                failure {
+                    
+                    sendTo {
+                        recipientList()
+                    }
+                }
+            }
+        }
+    
 
 
 
