88d827747c1b67c9d1dac1306fe36c99abff12b4
Mustafa <54718866+mdmustafa1@users.noreply.github.com>
2020-07-22 16:27:50 +0530

    cjf7

--- jenk_file.groovy
+++ jenk_file.groovy
@@ -3,9 +3,11 @@
 job('job1_pull_build'){
      scm{
              github('https://github.com/mdmustafa1/devops_task6.git')      
-         }triggers{
-                upstream('job2_seed','SUCCESS')
-           }
+         } 
+     
+            triggers {
+                upstream('seed_job2', 'SUCCESS')
+                 }
     
     steps{shell('if ls | grep .html; then sudo docker build -t any0/phtml:v1 . -f html.Dockerfile; sudo docker push any0/phtml:v1;  elif ls | grep .php; then sudo docker build -t any0/php:v1 . -f php.Dockerfile; sudo docker push  any0/php:v1;  else echo "all builded"; fi')
        
