0359a10c9711ec1d6b2e8de23acbe319942143b9
Mustafa <54718866+mdmustafa1@users.noreply.github.com>
2020-07-22 16:37:06 +0530

    cjf7

--- jenk_file.groovy
+++ jenk_file.groovy
@@ -14,6 +14,18 @@ job('job1_pull_build'){
        }
 }
 
+//code for job2 deployment on kubernetes using various resourcses
+job('job2_deploy'){
+     triggers{
+            upstream('job1_pull_build','SUCCESS')
+         }
+     steps{
+ shell('cd /var/lib/jenkins/workspace/job2_seed/; if (ls | grep .html); then; if(sudo kubectl get deploy | grep htmldeploy); then; echo "html env already running"; kubectl rollout restart deploy/htmldeploy; kubectl rollout status deploy/htmldeploy; else; sudo kubectl apply -k /task3/html; fi; fi; if (ls | grep .php); then; if(sudo kubectl get deploy | grep phpdeploy); then; echo "php env already running"; kubectl rollout restart deploy/phpdeploy; kubectl rollout status deploy/phpdeploy; else; sudo kubectl apply -k /task3/php; fi; fi')
+        }}
+
+
+
+
 
        
       
